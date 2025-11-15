b85772eeafb1eb89bed3a26c9d93149f5c7ce42a
Igor Gabriel <igor.gabrieljf05@gmail.com>
2025-11-15 18:29:09 -0300

    Add files via upload

--- home/function.php
+++ home/function.php
@@ -4,4 +4,4 @@
 $bancodados_arquivo = file_get_contents("../BancoDeDados.json");
 $bancodados = json_decode($bancodados_arquivo, true);
 
-echo json_encode(["database"=> $bancodados]);
+echo json_encode(["database"=> json_encode($bancodados)])
